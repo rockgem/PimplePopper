1828a7b6d2add6c97eb7556d741c5ac825d7271a
rockgem <63050183+rockgem@users.noreply.github.com>
2023-02-26 09:07:38 +0800

    added new languages
    dont forget to re-impor translation.csv file

--- menu/new_main_screen.gd
+++ menu/new_main_screen.gd
@@ -15,6 +15,9 @@ var langs = [
 	['zh_TW', 'K_CN_TRAD_SELECTION'],
 	['ru', 'K_RUSSIAN_SELECTION'],
 	['sr', 'K_SERBIAN_SELECTION'],
+	['fr', 'K_FRENCH_SELECTION'],
+	['it', 'K_ITALIAN_SELECTION'],
+	['pt_br', 'K_PT_SELECTION'],
 ]
 
 
